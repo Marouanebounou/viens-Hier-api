af8de679e81428565e6199c9e4941b7c36ad3ff6
marouane bounou <maroinbounou.123@gmail.com>
2025-07-29 14:46:34 +0100

    update urls

--- index.js
+++ index.js
@@ -10,14 +10,14 @@ app.use(express.json());
 
 connectDB();
 
-// const corsOptions = {
-//   origin: process.env.NODE_ENV === 'production' 
-//     ? [process.env.BASE_URL] 
-//     : ["https://vienshier.netlify.app/"],  
-//   credentials: true,
-//   allowedHeaders: ["Content-Type", "Authorization"],
-// };
-
+{/*const corsOptions = {
+  origin: process.env.NODE_ENV === 'production' 
+    ? [process.env.BASE_URL] 
+    : ["http://localhost:5173"],  
+  credentials: true,
+  allowedHeaders: ["Content-Type", "Authorization"],
+};
+*/}
 app.use(cors());
 app.use('/api', invitation);
 
